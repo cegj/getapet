7b68c6c2bdb08e13ff269621a5c150d2e997dc89
cegj <cegasparjr@gmail.com>
2022-07-05 19:13:58 -0300

    First commit

---
+++ backend/index.mjs
@@ -0,0 +1,18 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+// Config JSON response
+app.use(express.json())
+
+// Solve CORS
+app.use(cors({ credentials: true, origin: 'http://localhost:3000'}))
+
+// Public folder for images
+app.use(express.static('public'))
+
+// Routes
+
+// Start
+app.listen(5000)
